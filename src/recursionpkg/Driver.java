81c63b99c309b03dba22f795dc99203b1192d349
Ryan Rasti <diamonddelt@gmail.com>
2015-02-19 03:29:11 -0800

    Removed console output to focus on adding recursion problems. I guess I need to study IO again.

--- src/recursionpkg/Driver.java
+++ src/recursionpkg/Driver.java
@@ -7,30 +7,6 @@ public class Driver {
 	// credit goes to codingbat.com for allowing me to practice these examples
 	public static void main(String[] args) {
 		
-		List<String> recursionExamples = Arrays.asList(
-				"factorial"
-				);
-		
-		System.out.println("Please select the recursion example you wish to test:");
-		
-		Scanner scanner = new Scanner(System.in);
-		String selection = scanner.nextLine();
-		
-		selection = selection.toLowerCase();
-		switch (selection) {
-			case ("factorial"):
-				Factorial factorial = new Factorial();
-			
-				System.out.println(factorial.displayProblem());
-				
-				int input = Integer.parseInt(scanner.next());
-				System.out.println(factorial.displaySolution(input));
-				break;
-			default:
-				System.out.println("Please select a supported example.");
-				break;
-		}
-		
-		scanner.close();
+		// nothing here yet - decided to remove output until recursion practice is complete
 	}
 }
--- src/recursionpkg/Factorial.java
+++ src/recursionpkg/Factorial.java
@@ -2,37 +2,14 @@ package recursionpkg;
 
 public class Factorial {
 	
-	private String solutionResults;
-	
-	// displays initial recursion problem
-	public String displayProblem() {
-		String problem = 
-			"Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... \n" + 
-			"1. Compute the result recursively (without loops). \n" +
-			"factorial(1) → 1\n" + 
-			"factorial(2) → 2\n" +
-			"factorial(3) → 6\n";
-		return problem;
-	}
-	
-	public String displaySolution(int n) {
-		solutionResults += "Generating output from factorial recursion based on input: " + n + "\n";
-		// execute factorial recursion
-		factorial(n);
-
-		return solutionResults;
-	}
-	
 	private int factorial(int n) {
 		if (n == 1) {
 			// define terminal case i.e. where the recursion will stop
-			solutionResults += "Returning " + n + "because termial case was encountered.\n";
 			return n;
 		}
 		else {
 			// assume the recursive result produces a correct value
 			// and change what it returns to get the answer you need
-			solutionResults += "Returning " + n + ".\n";
 			return n * (factorial(n - 1));
 		}
 	}
